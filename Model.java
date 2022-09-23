199e76c09a7b720a35bc44a9abf014383201ba0e
blixtcorp <jakobpersson007@gmail.com>
2022-09-23 08:48:55 +0200

    comments

--- Model.java
+++ Model.java
@@ -29,7 +29,7 @@ class Model {
 	}
 
 	void step(double deltaT) {
-		// TODO this method implements one step of simulation with a step deltaT
+
 		Ball b0 = balls[0];
 		Ball b1 = balls[1];
 		double g = 9.82;
@@ -44,6 +44,7 @@ class Model {
 				b.x = b.radius;
 				b.vx *= -1;
 			}
+			//Cool stuff to unstuck ball in wall
 			else if (b.y > areaHeight - b.radius) {
 				b.y = areaHeight - b.radius;
 				b.vy *= -1;
@@ -69,7 +70,7 @@ class Model {
 
 		//Check if the squared distance between the (x,y)-coordinates of the balls is smaller than the squared sum of the radii
 		//smaller distance indicates collision
-		boolean collision = distanceSq < (b0.radius + b1.radius) * (b0.radius + b1.radius);
+		boolean collision = distanceSq < ((b0.radius + b1.radius) * (b0.radius + b1.radius));
 
 		double speedX = b1.vx - b0.vx;
 		double speedY = b1.vy - b0.vy;
@@ -92,17 +93,18 @@ class Model {
 		double b0vy = b0.vy * Math.cos(alpha) - b0.vx * Math.sin(alpha);
 
 		double b1vx = b1.vx * Math.cos(alpha) + b1.vy * Math.sin(alpha);
-		double b1vy = b1.vy * Math.cos(alpha) - b1.vy * Math.sin(alpha);
+		double b1vy = b1.vy * Math.cos(alpha) - b1.vx * Math.sin(alpha);
 
-		double[] impact1 = collision(b0vx, b1vx, b0.m, b1.m);
-		double[] impact2 = collision(b0vy, b1vy, b0.m, b1.m);
+		//Computes the post-collision velocities in "vector" form.
+		double[] impactX = collision(b0vx, b1vx, b0.m, b1.m);
+		double[] impactY = collision(b0vy, b1vy, b0.m, b1.m);
 
 		//Rotate back to the original orientation
-		double newB0vx = impact1[0] * Math.cos(alpha) - b0vy * Math.sin(alpha);
-		double newB0vy = impact1[1] * Math.sin(alpha) + b0vy * Math.cos(alpha);
+		double newB0vx = impactX[0] * Math.cos(alpha) - b0vy * Math.sin(alpha);
+		double newB0vy = impactY[0] * Math.sin(alpha) + b0vy * Math.cos(alpha);
 
-		double newB1vx = impact2[0] * Math.cos(alpha) - b1vy * Math.sin(alpha);
-		double newB1vy = impact2[1] * Math.sin(alpha) + b1vy * Math.cos(alpha);
+		double newB1vx = impactX[1] * Math.cos(alpha) - b1vy * Math.sin(alpha);
+		double newB1vy = impactY[1] * Math.sin(alpha) + b1vy * Math.cos(alpha);
 
 		//Update the velocities
 		b0.vx = newB0vx;
@@ -113,6 +115,7 @@ class Model {
 	}
 
 	//Math taken from https://en.wikipedia.org/wiki/Elastic_collision
+	//Returns an array that contains the new velocity for each ball
 	double[] collision(double b0U, double b1U, double m0, double m1){
 
 		double b0V = (m0 - m1)/(m0 + m1) * b0U+ (2 * m1)/(m0 + m1) * b1U;
